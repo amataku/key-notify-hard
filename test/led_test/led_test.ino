cb8f2e153a9e9708d724780dc109cdaf84877b3f
amataku <amataku13@gmail.com>
2017-11-13 19:56:22 +0900

    add off

--- test/led_test/led_test.ino
+++ test/led_test/led_test.ino
@@ -4,18 +4,25 @@ bool flag=false;
 
 void setup() {
   pinMode(led_out,OUTPUT);
+  Serial.begin(9800);
 }
 
 void loop() {
   int press;
 
   press = analogRead(ana_in);
+
+  Serial.println(press);
   
-  if(press <= 1000){
+  if(press < 1010){
     flag=true;
   }
 
   if(flag){
     digitalWrite(led_out,HIGH);
+    flag=false;
+  }else{
+    digitalWrite(led_out,LOW);
   }
+  
 }
