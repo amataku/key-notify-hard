9271aed340820f2ad5c4303ba02bea3a4727b687
amataku <amataku13@gmail.com>
2018-04-26 19:23:45 +0900

    add CHANNEL2

--- test/raspberry_test.py
+++ test/raspberry_test.py
@@ -2,25 +2,32 @@ import RPi.GPIO as GPIO
 import time
 import requests
 
-CHANNEL = 18
-DELAYTIME = 50000
+CHANNEL_1 = 18
+CHANNEL_2 = 17
+DELAYTIME = 50
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(CHANNEL,GPIO.IN)
+GPIO.setup(CHANNEL_1,GPIO.IN)
+GPIO.setup(CHANNEL_2,GPIO.IN)
 
-before = 3
+before_1 = 3
+before_2 = 3
 delay = 0
 
 while True:
-    input = GPIO.input(CHANNEL)
+    input_1 = GPIO.input(CHANNEL_1)
+    input_2 = GPIO.input(CHANNEL_2)
+    print str(input_2)
     if delay == 0:
-        if input != before:
+        if input_2 != before_2:
             delay = 1
     if delay != 0:
-        if input == before:
+        if input_2 == before_2:
             delay = delay + 1
+    else:
+        delay = 0
     if delay == DELAYTIME:
-        if input == 0:
+        if input_1 == 0 or input_2 == 0:
             url = "https://key-notify-server.herokuapp.com/api/hard/on"
             requests.post(url,data={})
             print "on!"
@@ -30,5 +37,6 @@ while True:
             requests.post(url,data={})
             print "off!"
             delay = 0
-    before = input
+    before_1 = input_2
+    before_2 = input_2
 GPIO.cleanup()
