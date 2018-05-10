8cbcbb1a92829abdd674297849fbd3cd897722a9
amataku <amataku13@gmail.com>
2018-05-10 22:08:59 +0900

    add sh

--- test/raspberry_test.py
+++ test/raspberry_test.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import requests
 import json
+import os
 
 CHANNEL_1 = 18
 CHANNEL_2 = 17
@@ -14,7 +15,7 @@ GPIO.setup(CHANNEL_2,GPIO.IN)
 APP_ID = os.environ["KEY_NOTIFY"]
 
 payload = {'app_id': APP_ID}
-headrs = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json'}
 
 before_1 = 3
 before_2 = 3
@@ -23,8 +24,6 @@ delay = 0
 while True:
     input_1 = GPIO.input(CHANNEL_1)
     input_2 = GPIO.input(CHANNEL_2)
-    print str(input_2)
-    print str(input_1)
     if delay == 0:
         if input_2 != before_2 or input_1 != before_1:
             delay = 1
@@ -36,12 +35,12 @@ while True:
     if delay == DELAYTIME:
         if input_2 == 0 or input_1 == 0:
             url = "https://key-notify-server.herokuapp.com/api/hard/on"
-            requests.post(url,data=json.dumps(payload),headrs=headrs)
+            requests.post(url,data=json.dumps(payload),headers=headers)
             print "on!"
             delay = 0
         else:
             url = "https://key-notify-server.herokuapp.com/api/hard/off"
-            requests.post(url,data=json.dumps(payload),headrs=headrs)
+            requests.post(url,data=json.dumps(payload),headers=headers)
             print "off!"
             delay = 0
     before_1 = input_1
